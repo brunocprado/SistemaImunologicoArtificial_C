e1051a5c93235bb5b4b74d647bdb606dc5ae5126
brunocprado <bruno_cesar_l@hotmail.com>
2018-01-31 21:35:00 -0200

    :recycle: Patogeno.virus

--- celulas/patogeno.cpp
+++ celulas/patogeno.cpp
@@ -1,9 +1,13 @@
 #include "patogeno.h"
 
 Patogeno::Patogeno() : Celula(PATOGENO){
+    this->virus = new Virus("TESTE");
+}
 
+Patogeno::Patogeno(Virus *virus) : Celula(PATOGENO){
+    this->virus = virus;
 }
 
 void Patogeno::loop(){
-
+    //TODO
 }
--- celulas/patogeno.h
+++ celulas/patogeno.h
@@ -2,12 +2,17 @@
 #define PATOGENO_H
 
 #include "celulas/celula.h"
+#include "estatisticas/virus.h"
 
 class Patogeno : public Celula{
 public:
     Patogeno();
+    Patogeno(Virus* virus);
 
     void loop();
+
+private:
+    Virus* virus;
 };
 
 #endif // PATOGENO_H
