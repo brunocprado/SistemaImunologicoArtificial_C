386a2e17ddcc58e48f2d484c5107642b08f0ec0b
brunocprado <bruno_cesar_l@hotmail.com>
2018-02-01 00:12:10 -0200

    :new: Patogeno.loop v1

--- celulas/patogeno.cpp
+++ celulas/patogeno.cpp
@@ -1,13 +1,43 @@
 #include "patogeno.h"
+#include "sistemaimunologico.h"
 
 Patogeno::Patogeno() : Celula(PATOGENO){
     this->virus = new Virus("TESTE");
+    virus->add();
 }
 
 Patogeno::Patogeno(Virus *virus) : Celula(PATOGENO){
     this->virus = virus;
+    virus->add();
 }
 
 void Patogeno::loop(){
-    //TODO
+
+    if(prox != nullptr){
+        if(calculaDistancia(prox) < 6){
+            processando = true;
+        } else {
+            move(prox);
+        }
+
+        return;
+    }
+
+    double maisProx = INT16_MAX;
+    double dist = 0;
+
+    QList<Celula*>::iterator i = SistemaImunologico::getInstancia()->getCelulas()->begin();
+    for (; i != SistemaImunologico::getInstancia()->getCelulas()->end(); ++i){
+        if((*i)->getTipo() == PATOGENO) continue;
+        dist = calculaDistancia(*i);
+        if(maisProx > dist){
+            maisProx = dist;
+            prox = *i;
+        }
+    }
+
+    printf("PATOGENO (x,y) = %d %d \n",x,y);
+    printf("ALVO (x,y) = %d %d \n",prox->x,prox->y);
+
+    move(prox);
 }
