4172475fb5702afafda06ac09e8e62465dd60b9b
brunocprado <bruno_cesar_l@hotmail.com>
2018-02-05 01:19:44 -0200

    :new: Neutrofilo

--- celulas/neutrofilo.cpp
+++ celulas/neutrofilo.cpp
@@ -1,3 +1,4 @@
+#include "sistemaimunologico.h"
 #include "neutrofilo.h"
 
 Neutrofilo::Neutrofilo() : Celula(NEUTROFILO){
@@ -5,5 +6,5 @@ Neutrofilo::Neutrofilo() : Celula(NEUTROFILO){
 }
 
 void Neutrofilo::loop(){
-
+    emit SistemaImunologico::getInstancia()->movimentaCelula(this->id,x + rand() % 4 - 2,y + rand() % 4 - 2);
 }
