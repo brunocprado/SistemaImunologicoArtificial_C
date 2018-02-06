a5f682890e910e97a0eed2a9e03d69672f3f9104
brunocprado <bruno_cesar_l@hotmail.com>
2018-02-06 14:30:26 -0200

    Macrofago.interator -> Index

--- celulas/macrofago.cpp
+++ celulas/macrofago.cpp
@@ -9,24 +9,23 @@ Macrofago::Macrofago() : Celula(MACROFAGO){
 void Macrofago::loop(){
     if(estado == FAGOCITANDO) return;
 
-    QList<CompostoQuimico*>::iterator i;
-    for (i = SistemaImunologico::getInstancia()->getQuimica()->getCompostos()->begin(); i != SistemaImunologico::getInstancia()->getQuimica()->getCompostos()->end(); ++i){
-        if((*i)->getTipo() != CompostoQuimico::PAMP && (*i)->getTipo() != CompostoQuimico::CITOCINA) continue;
-
-        CompostoQuimico* composto = (*i);
+//    QList<CompostoQuimico*>::iterator i;
+    for (int i = 0; i < SistemaImunologico::getCompostos()->length(); i++){
+        CompostoQuimico *composto = SistemaImunologico::getCompostos()->at(i);
+        if(composto->getTipo() != CompostoQuimico::PAMP && composto->getTipo() != CompostoQuimico::CITOCINA) continue;
 
         if(composto->getEmissor() == nullptr) continue;
 //        if(composto->getEmissor() == nullptr) continue; //REPENSAR SOBRE DPS !! if(composto.getEmissor() != null && !celulas.contains(composto.getEmissor())) continue;
 
         double dist = calculaDistancia(composto->x,composto->y);
 
-        if(dist <= composto->getRaio() + 6){
+        if(dist <= composto->getRaio() + 5){
 //            printf("%f\n",dist);
             alvo = composto->getEmissor();
             tempoDetectado = QDateTime::currentDateTime();
 
 //            if(dist <= 4) fagocita();
-            if(estado == REPOUSO) { emiteQuimica(CompostoQuimico::CITOCINA,5);
+            if(estado == REPOUSO) { emiteQuimica(CompostoQuimico::CITOCINA,6);
                 estado = ATIVO; }
             move(alvo);
         }
