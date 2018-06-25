7b36a8602fa516b56fb344c68b5b1c4c10bb487d
Bruno Prado <bruno_cesar_l@hotmail.com>
2018-06-25 17:30:44 -0300

    Comum::get|set Virus

--- celulas/comum.cpp
+++ celulas/comum.cpp
@@ -5,6 +5,7 @@ Comum::Comum() : Celula(TIPO_CELULA::COMUM){
 }
 
 void Comum::loop(){
+    moveRand();
     return;
 //    if(estado == ESTADO::INFECTADA) printf("INFECTADO %d",id);
 }
@@ -20,6 +21,7 @@ void Comum::setEstado(Celula::ESTADO e){
 QString Comum::extra(){
     QJsonObject json;
     json.insert("estado",QVariant::fromValue(estado).value<QString>());
+    if(virus) json.insert("virus",virus->getIdentificador());
 
     QJsonDocument tmp(json);
     return tmp.toJson();
--- celulas/comum.h
+++ celulas/comum.h
@@ -1,6 +1,7 @@
 #ifndef COMUM_H
 #define COMUM_H
 
+#include "estatisticas/virus.h"
 #include "celulas/celula.h"
 
 class Comum : public Celula {
@@ -12,10 +13,13 @@ public:
     void loop();
     ESTADO getEstado();
     void setEstado(Celula::ESTADO);
+    void setVirus(Virus* v){virus=v;}
+    Virus* getVirus(){return virus;}
     QString extra();
 
 private:
     ESTADO estado = ESTADO::REPOUSO;
+    Virus* virus = nullptr;
 
 };
 
