b6451024204c33b244779b54045111888a9c1dbe
Bruno Prado <bruno_cesar_l@hotmail.com>
2018-06-27 21:16:49 -0300

    Anticorpo v1

--- celulas/anticorpo.cpp
+++ celulas/anticorpo.cpp
@@ -1,10 +1,27 @@
-#include "anticorpo.h"
+#include "sistemaimunologico.h"
+#include "celulas/anticorpo.h"
+#include "celulas/patogeno.h"
 
 Anticorpo::Anticorpo(double x,double y,Virus* v) : Celula(TIPO_CELULA::ANTICORPO,x,y){
     virus = v;
 }
 
 void Anticorpo::loop(){
+
+    QList<Celula*>* tmp = SistemaImunologico::getInstancia()->getCelulas();
+    for (int i = 0; i < tmp->length(); i++){
+        Celula* celula = tmp->at(i);
+
+        if(celula->getTipo() != TIPO_CELULA::PATOGENO) continue;
+
+        Patogeno* tmp = static_cast<Patogeno*>(celula);
+        if(calculaDistancia(celula) <= 10 && !tmp->suprimido && tmp->getVirus() == virus){
+            tmp->suprimido = true;
+        }
+    }
+
+    //TODO REMOVE
+
     moveRand(2);
 }
 
